ff3b678e36f348991d26a58eb8c50ce1138ba55c
Nick (Partixel) Lock <partixel@v-handle.com>
2020-07-28 12:52:35 +0100

    Only duplicate if not in studio

--- MainModule.ModuleScript.lua
+++ MainModule.ModuleScript.lua
@@ -1,3 +1,7 @@
-local Module = script:WaitForChild("CoroutineErrorHandling")
-Module.Parent = game:GetService("ReplicatedStorage")
-return require(Module)
+if game:GetService("RunService"):IsRunning() then
+	local Module = script:WaitForChild("CoroutineErrorHandling"):Clone()
+	Module.Parent = game:GetService("ReplicatedStorage")
+	return require(Module)
+else
+	return require(script.CoroutineErrorHandling)
+end
